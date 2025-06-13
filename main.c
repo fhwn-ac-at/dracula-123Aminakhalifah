3a7c6a26610f79f4228b9d846d35a346ca53be54
123Aminakhalifah <162569620+123Aminakhalifah@users.noreply.github.com>
2025-06-14 00:08:29 +0200

    main.c

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 #include "dice.h"
 #include "simulator.h"
 #include "stats.h"
-#include "config.h"  
 
+#include "config.h"  
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         printf("Usage: %s <board_config_file>\n", argv[0]);
